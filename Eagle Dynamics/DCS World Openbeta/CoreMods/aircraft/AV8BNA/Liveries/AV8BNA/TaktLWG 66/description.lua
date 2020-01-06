fdefb50dfbbf28568f6b2b19e20c12595718727c
DominikJosh <42120789+DominikJosh@users.noreply.github.com>
2020-01-06 06:44:58 +0100

    Add files via upload

---
+++ Eagle Dynamics/DCS World Openbeta/CoreMods/aircraft/AV8BNA/Liveries/AV8BNA/TaktLWG 66/description.lua
@@ -0,0 +1,51 @@
+livery = {
+
+	{"AV8B1", 0 ,"AV8_T1_BN",false};
+	{"AV8B2", 0 ,"AV8_T2_BN",false};
+	{"AV8B3", 0 ,"AV8_T3_BN",false};
+	{"AV8B4", 0 ,"AV8_T4_BN",false};
+	{"AV8B7", 0 ,"AV8_T7_BN",false};
+	{"AV8B8", 0 ,"AV8_T8_BN",false};
+	{"AV8B_WV", 0 ,"AV8_T4_BN",false};
+	{"Pilot_AV8B_gear", 0 ,"Pilot_AV8B_gear_BN",false};
+	{"pilot_AV8B_body", 0 ,"pilot_AV8B_BN",false};
+		
+	{"AV8B_T1_FUSE1", 0 ,"AV8_T1_BN",false};
+	{"AV8B_T1_FUSE1", DECAL ,"AV8_TNumbers_BN",false};
+	
+	{"AV8B_T1_FUSE10", 0 ,"AV8_T1_BN",false};
+	{"AV8B_T1_FUSE10", DECAL ,"AV8_TNumbers_BN",false};
+	
+	{"AV8B_T2_FUSE1", 0 ,"AV8_T2_BN",false};
+	{"AV8B_T2_FUSE1", DECAL ,"AV8_TNumbers_BN",false};
+	
+	{"AV8B_T2_FUSE10", 0 ,"AV8_T2_BN",false};
+	{"AV8B_T2_FUSE10", DECAL ,"AV8_TNumbers_BN",false};
+	
+	{"AV8B_T3_FUSE1", 0 ,"AV8_T3_BN",false};
+	{"AV8B_T3_FUSE1", DECAL ,"AV8_TNumbers_BN",false};
+	
+	{"AV8B_T3_FUSE10", 0 ,"AV8_T3_BN",false};
+	{"AV8B_T3_FUSE10", DECAL ,"AV8_TNumbers_BN",false};
+	
+	{"AV8B_T3_TAIL1", 0 ,"AV8_T3_BN",false};
+	{"AV8B_T3_TAIL1", DECAL ,"AV8_TNumbers_BN",false};
+	
+	{"AV8B_T3_TAIL10", 0 ,"AV8_T3_BN",false};
+	{"AV8B_T3_TAIL10", DECAL ,"AV8_TNumbers_BN",false};
+	
+	{"AV8B_T3_TAIL100", 0 ,"AV8_T3_BN",false};
+	{"AV8B_T3_TAIL100", DECAL ,"AV8_TNumbers_BN",false};
+	
+	{"pilot_AV8B_patch", 0 ,"empty",true};
+	{"AV8B1", ROUGHNESS_METALLIC ,"AV8_T1_BN",true};
+	{"AV8B2", ROUGHNESS_METALLIC ,"AV8_T2_BN",true};
+	{"AV8B3", ROUGHNESS_METALLIC ,"AV8_T3_BN",true};
+	{"AV8B4", ROUGHNESS_METALLIC ,"AV8_T4_BN",true};
+	{"AV8B7", ROUGHNESS_METALLIC ,"AV8_T7_BN",true};
+	{"AV8B8", ROUGHNESS_METALLIC ,"AV8_T8_BN",true};
+
+}
+
+name = "TaktLWG 66"
+
