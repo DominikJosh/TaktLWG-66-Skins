449aa3cea7a6d80cf5592ac7d035d4ba7b48a443
DominikJosh <42120789+DominikJosh@users.noreply.github.com>
2020-01-10 09:19:59 +0100

    KC-135MPRS Upload

---
+++ Eagle Dynamics/DCS World Openbeta/CoreMods/aircraft/AV8BNA/Liveries/KC135MPRS/TaktLWG 66/description.lua
@@ -0,0 +1,11 @@
+livery = {
+	{"kc_135_body_1", 0 ,"kc_135_body_1",false};
+	{"kc_135_body_2", 0 ,"kc_135_body_2",false};
+	{"kc_135_wings", 0 ,"kc_135_wings",false};
+	{"kc_135_wings_mark", 0 ,"kc_135_wings",false};
+	{"kc_135_wings_mark", 3 ,"empty",true};
+	{"kc_135_elements", 0 ,"kc_135_elements",false};	
+	{"kc_135_notes", 0 ,"empty",true};
+	{"pilot C-130", 0 ,"kc-135_pilots",false};
+}
+name = "TaktLWG 66"
