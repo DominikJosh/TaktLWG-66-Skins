9d8ebf1e08796337ca9b26bc8d09b42b696d1f7d
DominikJosh <42120789+DominikJosh@users.noreply.github.com>
2020-01-10 09:37:45 +0100

    Update description.lua

--- Eagle Dynamics/DCS World Openbeta/CoreMods/aircraft/AV8BNA/Liveries/KC130/TaktLWG 66/description.lua
+++ Eagle Dynamics/DCS World Openbeta/CoreMods/aircraft/AV8BNA/Liveries/KC130/TaktLWG 66/description.lua
@@ -1,8 +1,8 @@
 livery = {
-	{"C-130_fusel", 0 ,"C-130_map_fus_Takt",false};
-	{"C-130_wing", 0 ,"C-130_map_wings_1_Takt",false};
-	{"C-130_wing_2", 0 ,"C-130_map_wings_2_Takt",false};
-	{"C-130_notes", 0 ,"C-130_notes_empty",true};
+	{"KC-130_fusel", 0 ,"C-130_map_fus_Takt",false};
+	{"KC-130_wing", 0 ,"C-130_map_wings_1_Takt",false};
+	{"KC-130_wing_2", 0 ,"C-130_map_wings_2_Takt",false};
+	{"KC-130_notes", 0 ,"C-130_notes_empty",true};
 	
         {"pilots_C-130", 0 ,"IRIAF_pilots2_C-130",false};
 }
