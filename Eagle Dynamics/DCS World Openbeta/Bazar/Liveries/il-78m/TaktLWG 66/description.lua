5e4b774b3b97f73f2fb86e7e212e7f7394aa9e9f
DominikJosh <42120789+DominikJosh@users.noreply.github.com>
2020-01-10 09:11:51 +0100

    IL78M Upload

---
+++ Eagle Dynamics/DCS World Openbeta/Bazar/Liveries/il-78m/TaktLWG 66/description.lua
@@ -0,0 +1,12 @@
+livery = {
+	--- by uboats
+    {"76MD-CONE-01", 0, "upaz-cone-01", false};
+	{"IL78-MAP-01", 0,"IL78-SKIN01-01",false};
+	{"IL78-MAP-02", 0,"IL78-SKIN01-02",false};
+    {"IL78-MAP-03",  0, "IL78-SKIN01-03", false};
+	{"78M-SKIN01-02", 0,"IL78-SKIN01-02",false};
+    {"78M-SKIN01-03", 0, "IL78-SKIN01-03", false};
+	{"78M-SKIN01-05", 0,"empty",true};
+    {"pilot_ IL76", 0, "crew_il76", false};
+}
+name = "TaktLWG 66"
