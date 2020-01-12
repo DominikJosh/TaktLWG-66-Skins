daa196c48ee428b94f704146e97cca555aa89967
DominikJosh <42120789+DominikJosh@users.noreply.github.com>
2020-01-12 13:43:51 +0100

    Add files via upload

---
+++ Eagle Dynamics/DCS World Openbeta/Bazar/Liveries/b-1b/TaktLWG 66/description.lua
@@ -0,0 +1,9 @@
+livery = {
+	{"texture B-1B-OKR.bmp f2 g0", 0, "B-1B-Takt", false};
+	{"texture USAF.tga f2 g0", 0, "empty", true};
+	{"texture nomera.tga f2 g0",0,"empty",true};
+	{"texture USAF.tga f0 g0", 0, "empty", true};
+	{"texture nomera.tga f0 g0", 0, "empty", true};
+	{"texture B-1B-OKR.bmp f0 g0", 0, "B-1B-Takt", false};
+}
+name = "TaktLWG 66"
