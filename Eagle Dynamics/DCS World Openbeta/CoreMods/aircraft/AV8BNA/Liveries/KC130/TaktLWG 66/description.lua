a495d4f1996cd55d77a961b34107dcdb07e01a5a
Dominik Schupp <dante_josh_devil@web.de>
2020-01-12 15:21:54 +0100

    Upload new

---
+++ Eagle Dynamics/DCS World Openbeta/CoreMods/aircraft/AV8BNA/Liveries/KC130/TaktLWG 66/description.lua
@@ -0,0 +1,10 @@
+livery = {
+	{"KC-130_fusel", 0 ,"C-130_map_fus_Takt",false};
+	{"KC-130_wing", 0 ,"C-130_map_wings_1_Takt",false};
+	{"KC-130_wing_2", 0 ,"C-130_map_wings_2_Takt",false};
+	{"KC-130_notes", 0 ,"C-130_notes_empty",true};
+	
+        {"pilots_C-130", 0 ,"IRIAF_pilots2_C-130",false};
+}
+
+name = "TaktLWG 66"
