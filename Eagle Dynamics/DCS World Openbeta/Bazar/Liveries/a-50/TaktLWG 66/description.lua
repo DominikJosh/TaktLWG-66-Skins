26549f87d8fefd3e8c151ac3c6913a5306020e10
Dominik Schupp <dante_josh_devil@web.de>
2020-01-19 12:27:29 +0100

    a-50 added

---
+++ Eagle Dynamics/DCS World Openbeta/Bazar/Liveries/a-50/TaktLWG 66/description.lua
@@ -0,0 +1,11 @@
+livery = {
+	{"A50-MAP-01", 0 ,"A50-SKIN01-01",false};
+	{"A50-MAP-01", 2 ,"A50-SKIN01-01s",false};
+	{"A50-MAP-02", 0 ,"A50-SKIN01-02",false};
+	{"A50-MAP-03", 0 ,"A50-SKIN01-03",false};
+	{"A50-SKIN01-02", 0 ,"A50-SKIN01-02",false};
+	{"A50-SKIN01-03", 0 ,"1A50-SKIN01-03",false};
+	{"76MD-SKIN01-05", 0 ,"empty",true};
+		
+}
+name = "TaktLWG 66"
