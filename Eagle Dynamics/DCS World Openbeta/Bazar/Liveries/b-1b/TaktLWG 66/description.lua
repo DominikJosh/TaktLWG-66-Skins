55630ea866663ef4bf4d022de95ca726c841db20
DominikJosh <42120789+DominikJosh@users.noreply.github.com>
2020-02-17 19:23:11 +0100

    New

--- Eagle Dynamics/DCS World Openbeta/Bazar/Liveries/b-1b/TaktLWG 66/description.lua
+++
@@ -1,9 +0,0 @@
-livery = {
-	{"texture B-1B-OKR.bmp f2 g0", 0, "B-1B-Takt", false};
-	{"texture USAF.tga f2 g0", 0, "empty", true};
-	{"texture nomera.tga f2 g0",0,"empty",true};
-	{"texture USAF.tga f0 g0", 0, "empty", true};
-	{"texture nomera.tga f0 g0", 0, "empty", true};
-	{"texture B-1B-OKR.bmp f0 g0", 0, "B-1B-Takt", false};
-}
-name = "TaktLWG 66"
--- Eagle Dynamics/DCS World Openbeta/Bazar/Liveries/e-3a/TaktLwG 66/description.lua
+++
@@ -1,8 +0,0 @@
-livery = {
-	{"e-3_body_1", 0 ,"e-3_body_1",false};
-    {"e-3_body_2", 0 ,"e-3_body_2",false};
-    {"e-3_radar", 0 ,"e-3_radar",false};
-    {"e-3_wings_mark", 0 ,"e-3_wings",false};
-    {"e-3_wings_mark", 3 ,"e-3_notes",false};
-    {"e-3_wings", 0 ,"e-3_wings",false};
-}
