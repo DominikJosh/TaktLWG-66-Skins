f60f745f3fb794e889598cc88691ee9d93743802
DominikJosh <42120789+DominikJosh@users.noreply.github.com>
2020-02-17 19:26:05 +0100

    New Data input

---
+++ Eagle Dynamics/DCS World Openbeta/Bazar/Liveries/P-51D/TaktLWG 66 - Alkman/description.lua
@@ -0,0 +1,78 @@
+livery = {
+	{"P51D_FUZ_FRONT", 0 ,"p51D_DIF_Fuz_Front",false};
+	{"P51D_FUZ_REAR", 0 ,"p51D_DIF_Fuz_Rear",false};
+	{"P51D_WING_L", 0 ,"p51D_DIF_L_Wing",false};
+	{"P51D_WING_R", 0 ,"p51D_DIF_R_Wing",false};
+	
+	{"P51D_BORT_NUMBER_COCPIT_01", 0 ,"p51D_DIF_Fuz_Front",false};                 
+	{"P51D_BORT_NUMBER_COCPIT_01", 2 ,"p51D_DIF_Fuz_Front",false};                
+	{"P51D_BORT_NUMBER_COCPIT_01", DECAL ,"empty",true};                           
+	
+	
+	
+	{"P51D_BORT_NUMBER_FRONT_10", 0 ,"p51D_DIF_Fuz_Front",false};                  
+	{"P51D_BORT_NUMBER_FRONT_10", 2 ,"p51D_DIF_Fuz_Front",false};                 
+	{"P51D_BORT_NUMBER_FRONT_10", DECAL ,"empty",true};                                   
+
+	{"P51D_BORT_NUMBER_FRONT_01", 0 ,"p51D_DIF_Fuz_Front",false};                  
+	{"P51D_BORT_NUMBER_FRONT_01", 2 ,"p51D_DIF_Fuz_Front",false};
+	{"P51D_BORT_NUMBER_FRONT_01", DECAL ,"empty",true};
+	
+		
+	{"P51D_BORT_NUMBER_FRONT-2_10", 0 ,"p51D_DIF_Fuz_Front",false};                
+	{"P51D_BORT_NUMBER_FRONT-2_10", 2 ,"p51D_DIF_Fuz_Front",false};               
+	{"P51D_BORT_NUMBER_FRONT-2_10", DECAL ,"empty",true};
+	
+	{"P51D_BORT_NUMBER_FRONT-2_01", 0 ,"p51D_DIF_Fuz_Front",false};
+	{"P51D_BORT_NUMBER_FRONT-2_01", 2 ,"p51D_DIF_Fuz_Front",false};
+	{"P51D_BORT_NUMBER_FRONT-2_01", DECAL ,"empty",true};
+	
+	{"P51D_BORT_NUMBER_REAR-2_10", 0 ,"p51D_DIF_Fuz_Rear",false};
+	{"P51D_BORT_NUMBER_REAR-2_10", 2 ,"p51D_DIF_Fuz_Rear",false};
+	{"P51D_BORT_NUMBER_REAR-2_10", DECAL ,"empty",true};
+	
+	{"P51D_BORT_NUMBER_REAR-2_01", 0 ,"p51D_DIF_Fuz_Rear",false};
+	{"P51D_BORT_NUMBER_REAR-2_01", 2 ,"p51D_DIF_Fuz_Rear",false};
+	{"P51D_BORT_NUMBER_REAR-2_01", DECAL ,"empty",true};
+	
+		
+	{"P51D_BORT_NUMBER_TAIL_01", 0 ,"p51D_DIF_Fuz_Rear",false};                 
+	{"P51D_BORT_NUMBER_TAIL_01", 2 ,"p51D_DIF_Fuz_Rear",false};                 
+	{"P51D_BORT_NUMBER_TAIL_01", DECAL ,"empty",true};
+	
+	{"P51D_BORT_NUMBER_FIN_01", 0 ,"p51D_DIF_Fuz_Rear",false};                   
+	{"P51D_BORT_NUMBER_FIN_01", 2 ,"p51D_DIF_Fuz_Rear",false};                 
+	{"P51D_BORT_NUMBER_FIN_01", DECAL ,"empty",true};
+	
+	
+	
+	
+	{"P51D_SERIAL_NUMBER_L_001", 0 ,"p51D_DIF_Fuz_Rear",false};                
+	{"P51D_SERIAL_NUMBER_L_001", 2 ,"p51D_SPEC_ISR_Fuz_Rear",false};                
+	{"P51D_SERIAL_NUMBER_L_001", DECAL ,"empty",true};
+	
+	{"P51D_SERIAL_NUMBER_L_010", 0 ,"p51D_DIF_Fuz_Rear",false};
+	{"P51D_SERIAL_NUMBER_L_010", 2 ,"p51D_SPEC_ISR_Fuz_Rear",false};
+	{"P51D_SERIAL_NUMBER_L_010", DECAL ,"empty",true};
+	
+	{"P51D_SERIAL_NUMBER_L_100", 0 ,"p51D_DIF_Fuz_Rear",false};
+	{"P51D_SERIAL_NUMBER_L_100", 2 ,"p51D_SPEC_ISR_Fuz_Rear",false};
+	{"P51D_SERIAL_NUMBER_L_100", DECAL ,"empty",true};
+	
+	{"P51D_SERIAL_NUMBER_R_001", 0 ,"p51D_DIF_Fuz_Rear",false};
+	{"P51D_SERIAL_NUMBER_R_001", 2 ,"p51D_SPEC_ISR_Fuz_Rear",false};
+	{"P51D_SERIAL_NUMBER_R_001", DECAL ,"empty",true};
+	
+	{"P51D_SERIAL_NUMBER_R_010", 0 ,"p51D_DIF_Fuz_Rear",false};
+	{"P51D_SERIAL_NUMBER_R_010", 2 ,"p51D_SPEC_ISR_Fuz_Rear",false};
+	{"P51D_SERIAL_NUMBER_R_010", DECAL ,"empty",true};
+	
+	{"P51D_SERIAL_NUMBER_R_100", 0 ,"p51D_DIF_Fuz_Rear",false};
+	{"P51D_SERIAL_NUMBER_R_100", 2 ,"p51D_SPEC_ISR_Fuz_Rear",false};
+	{"P51D_SERIAL_NUMBER_R_100", DECAL ,"empty",true};
+	
+	{"pilot_P51_patch", 0 ,"empty",false};
+        {"pilot_P51_helmet_b", 0 ,"pilot_P51_helmet_b",false};                                             
+	
+}
+name = "TaktLWG 66 - Alkman"
---
+++ Eagle Dynamics/DCS World Openbeta/CoreMods/aircraft/M-2000C/Liveries/M-2000C/TaktLWG 66 - Alkman/description.lua
@@ -0,0 +1,72 @@
+livery = {
+	{"ext1", 0 ,"M2KC_1T_TAKT",false};
+	{"ext2", 0 ,"M2KC_2T_TAKT",false};
+--	{"pilot_M2KC_helmet", 0 ,"Pilot_M2KC_helmet_NAME-HERE",false};
+-- 	{"pilot_M2KC", 0 ,"pilot_M2KC_EXP_TAN",true};
+--	{"pilot_M2KC_patch", 0 ,"pilot_M2KC_patch_UAE",false};
+
+	{"FUSE_NUMBER1_001", 0 ,"M2KC_1T_TAKT",false};
+	{"FUSE_NUMBER1_001", DECAL ,"M2KC_N_TAKT",false};
+
+	{"FUSE_NUMBER2_001", 0 ,"M2KC_1T_TAKT",false};
+	{"FUSE_NUMBER2_001", DECAL ,"M2KC_N_TAKT",false};
+
+	{"FUSE_NUMBER2_010", 0 ,"M2KC_1T_TAKT",false};
+	{"FUSE_NUMBER2_010", DECAL ,"empty",false};
+
+	{"TAIL_NUMBER1_001", 0 ,"M2KC_2T_TAKT",false};
+	{"TAIL_NUMBER1_001", DECAL ,"empty",false};
+
+	{"TAIL_NUMBER1_010", 0 ,"M2KC_2T_TAKT",false};
+	{"TAIL_NUMBER1_010", DECAL ,"empty",false};
+
+	{"TAIL_NUMBER1_100", 0 ,"M2KC_2T_TAKT",false};
+	{"TAIL_NUMBER1_100", DECAL ,"empty",false};
+
+	{"TAIL_NUMBER2_001", 0 ,"M2KC_2T_TAKT",false};
+	{"TAIL_NUMBER2_001", DECAL ,"empty",false};
+
+	{"TAIL_NUMBER2_010", 0 ,"M2KC_2T_TAKT",false};
+	{"TAIL_NUMBER2_010", DECAL ,"empty",false};
+
+	{"TAIL_NUMBER2_100", 0 ,"M2KC_2T_TAKT",false};
+	{"TAIL_NUMBER2_100", DECAL ,"empty",false};
+
+	{"TAIL_NUMBER3_001", 0 ,"M2KC_2T_TAKT",false};
+	{"TAIL_NUMBER3_001", DECAL ,"empty",false};
+
+	{"TAIL_NUMBER3_010", 0 ,"M2KC_2T_TAKT",false};
+	{"TAIL_NUMBER3_010", DECAL ,"empty",false};
+
+	{"TAIL_NUMBER3_100", 0 ,"M2KC_2T_TAKT",false};
+	{"TAIL_NUMBER3_100", DECAL ,"empty",false};
+
+	{"NOSE_NUMBER_001", 0 ,"M2KC_1T_TAKT",false};
+	{"NOSE_NUMBER_001", DECAL ,"M2KC_N_TAKT_BK",false};
+
+	{"NOSE_NUMBER_010", 0 ,"M2KC_1T_TAKT",false};
+	{"NOSE_NUMBER_010", DECAL ,"M2KC_N_TAKT_BK",false};
+
+	{"NOSE_NUMBER_100", 0 ,"M2KC_1T_TAKT",false};
+	{"NOSE_NUMBER_100", DECAL ,"M2KC_N_TAKT_BK",false};
+
+	{"WINGR_NUMBER_001", 0 ,"M2KC_1T_TAKT",false};
+	{"WINGR_NUMBER_001", DECAL ,"empty",false};
+
+	{"WINGR_NUMBER_010", 0 ,"M2KC_2T_TAKT",false};
+	{"WINGR_NUMBER_010", DECAL ,"empty",false};
+
+	{"WINGR_NUMBER_100", 0 ,"M2KC_2T_TAKT",false};
+	{"WINGR_NUMBER_100", DECAL ,"empty",false};
+
+	{"WINGL_NUMBER_001", 0 ,"M2KC_1T_TAKT",false};
+	{"WINGL_NUMBER_001", DECAL ,"empty",false};
+
+	{"WINGL_NUMBER_010", 0 ,"M2KC_1T_TAKT",false};
+	{"WINGL_NUMBER_010", DECAL ,"empty",false};
+
+	{"WINGL_NUMBER_100", 0 ,"M2KC_1T_TAKT",false};
+	{"WINGL_NUMBER_100", DECAL ,"empty",false};
+}
+name = "TaktLWG 66 - Alkman"
+
