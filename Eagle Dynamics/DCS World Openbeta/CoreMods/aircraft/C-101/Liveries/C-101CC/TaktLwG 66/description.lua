785baab5a0abaae59956b3849167ad7dfe67e63c
DominikJosh <42120789+DominikJosh@users.noreply.github.com>
2020-02-20 19:22:30 +0100

    C-101 Upload

---
+++ Eagle Dynamics/DCS World Openbeta/CoreMods/aircraft/C-101/Liveries/C-101CC/TaktLwG 66/description.lua
@@ -0,0 +1,123 @@
+livery = {
+----------------------------------------------------------------------c-101-fuse-1----------------------------------------------------------------------
+-----------default texture without damaged---------------------
+	{"C-101-FUSE_1_1", 0 ,"C-101-fuse-1_Takt",false};
+	{"C-101-FUSE_1_1",ROUGHNESS_METALLIC,"C-101-fuse-1-CC-CHILE_RoughMet",true};	
+-----------default texture---------------------	
+	{"C-101-FUSE_1_2", 0 ,"C-101-fuse-1_Takt",false};
+	{"C-101-FUSE_1_2",ROUGHNESS_METALLIC,"C-101-fuse-1-CC-CHILE_RoughMet",true};		
+-----------numeral AGA/MATACAN/CLAEX VERSION----------------
+	{"C-101-FUSE_1_3", 0 ,"C-101-fuse-1_Takt",false};--FIRST DIGIT
+	{"C-101-FUSE_1_3",ROUGHNESS_METALLIC,"C-101-fuse-1-CC-CHILE_RoughMet",true};		
+	{"C-101-FUSE_1_3", DECAL ,"empty",true};	
+	{"C-101-FUSE_1_4", 0 ,"C-101-fuse-1_Takt",false};--SECOND DIGIT
+	{"C-101-FUSE_1_4",ROUGHNESS_METALLIC,"C-101-fuse-1-CC-CHILE_RoughMet",true};		
+	{"C-101-FUSE_1_4", DECAL ,"empty",true};		
+-----------numeral PATRULLA AGUILA VERSION------------------
+	{"C-101-FUSE_1_5", 0 ,"C-101-fuse-1_Takt",false};--FIRST DIGIT
+	{"C-101-FUSE_1_5",ROUGHNESS_METALLIC,"C-101-fuse-1-CC-CHILE_RoughMet",true};		
+	{"C-101-FUSE_1_5", DECAL ,"empty",true};
+	{"C-101-FUSE_1_6", 0 ,"C-101-fuse-1_Takt",false};--SECOND DIGIT
+	{"C-101-FUSE_1_6",ROUGHNESS_METALLIC,"C-101-fuse-1-CC-CHILE_RoughMet",true};	
+	{"C-101-FUSE_1_6", DECAL ,"empty",true};	
+-----------numeral JORDANIA VERSION-------------------------
+	{"C-101-FUSE_1_7", 0 ,"C-101-fuse-1_Takt",false};--FIRST DIGIT
+	{"C-101-FUSE_1_7",ROUGHNESS_METALLIC,"C-101-fuse-1-CC-CHILE_RoughMet",true};	
+	{"C-101-FUSE_1_7", DECAL ,"empty",true};	
+	{"C-101-FUSE_1_8", 0 ,"C-101-fuse-1_Takt",false};--SECOND DIGIT
+	{"C-101-FUSE_1_8",ROUGHNESS_METALLIC,"C-101-fuse-1-CC-CHILE_RoughMet",true};	
+	{"C-101-FUSE_1_8", DECAL ,"empty",true};	
+-----------numeral CHILE VERSION----------------------------
+	{"C-101-FUSE_1_9", 0 ,"C-101-fuse-1_Takt",false};--FIRST DIGIT
+	{"C-101-FUSE_1_9",ROUGHNESS_METALLIC,"C-101-fuse-1-CC-CHILE_RoughMet",true};	
+	{"C-101-FUSE_1_9", DECAL ,"empty",true};	
+	{"C-101-FUSE_1_10", 0 ,"C-101-fuse-1_Takt",false};--SECOND DIGIT
+	{"C-101-FUSE_1_10",ROUGHNESS_METALLIC,"C-101-fuse-1-CC-CHILE_RoughMet",true};	
+	{"C-101-FUSE_1_10", DECAL ,"empty",true};	
+-----------numeral SPANISH TAIL VERSION----------------------------
+	{"C-101-FUSE_1_11", 0 ,"C-101-fuse-1_Takt",false};--FIRST DIGIT
+	{"C-101-FUSE_1_11",ROUGHNESS_METALLIC,"C-101-fuse-1-CC-CHILE_RoughMet",true};	
+	{"C-101-FUSE_1_11", DECAL ,"empty",true};	
+	{"C-101-FUSE_1_12", 0 ,"C-101-fuse-1_Takt",false};--SECOND DIGIT	
+	{"C-101-FUSE_1_12",ROUGHNESS_METALLIC,"C-101-fuse-1-CC-CHILE_RoughMet",true};		
+	{"C-101-FUSE_1_12", DECAL ,"empty",true};	
+-----------numeral SPANISH TAIL BOTTOM VERSION----------------------------
+	{"C-101-FUSE_1_13", 0 ,"C-101-fuse-1_Takt",false};--FIRST DIGIT
+	{"C-101-FUSE_1_13",ROUGHNESS_METALLIC,"C-101-fuse-1-CC-CHILE_RoughMet",true};	
+	{"C-101-FUSE_1_13", DECAL ,"empty",true};	
+	{"C-101-FUSE_1_14", 0 ,"C-101-fuse-1_Takt",false};--SECOND DIGIT
+	{"C-101-FUSE_1_14",ROUGHNESS_METALLIC,"C-101-fuse-1-CC-CHILE_RoughMet",true};	
+	{"C-101-FUSE_1_14", DECAL ,"empty",true};	
+
+-----------numeral HONDURAS/JORDANIA TAIL VERSION----------------------------
+	{"C-101-FUSE_1_15", 0 ,"C-101-fuse-1_Takt",false};--FIRST DIGIT
+	{"C-101-FUSE_1_15",ROUGHNESS_METALLIC,"C-101-fuse-1-CC-CHILE_RoughMet",true};	
+	{"C-101-FUSE_1_15", DECAL ,"c-101-numeral",true};
+	{"C-101-FUSE_1_16", 0 ,"C-101-fuse-1_Takt",false};--SECOND DIGIT
+	{"C-101-FUSE_1_16",ROUGHNESS_METALLIC,"C-101-fuse-1-CC-CHILE_RoughMet",true};	
+	{"C-101-FUSE_1_16", DECAL ,"c-101-numeral",true};
+-----------numeral Patrulla Aguila Rudder VERSION----------------------------
+	{"C-101-FUSE_1_17", 0 ,"C-101-fuse-1_Takt",false};--FIRST DIGIT
+	{"C-101-FUSE_1_17",ROUGHNESS_METALLIC,"C-101-fuse-1-CC-CHILE_RoughMet",true};	
+	{"C-101-FUSE_1_17", DECAL ,"empty",true};
+
+-----------Aluminium effect VERSION----------------------------
+	{"C-101-FUSE_1_18", 0 ,"C-101-fuse-1_Takt",false};--FIRST DIGIT
+	{"C-101-FUSE_1_18",ROUGHNESS_METALLIC,"C-101-fuse-1-CC-CHILE_RoughMet",true};	
+-----------numeral Patrulla Aguila Pilot Name VERSION----------------------------
+	{"C-101-FUSE_1_19", 0 ,"C-101-fuse-1_Takt",false};--FIRST DIGIT
+	{"C-101-FUSE_1_19",ROUGHNESS_METALLIC,"C-101-fuse-1-CC-CHILE_RoughMet",true};	
+	{"C-101-FUSE_1_19", DECAL ,"empty",true};			
+	
+----------------------------------------------------------------------C-101-fuse-2_USA_CC----------------------------------------------------------------------
+-----------default "WING" texture without damaged---------------------
+	{"C-101-FUSE_2_1", 0 ,"C-101-fuse-2_Takt",false};
+	{"C-101-FUSE_2_1",ROUGHNESS_METALLIC,"C-101-fuse-2-2-CC_RoughMet",true};	
+-----------default "WING" texture---------------------	
+	{"C-101-FUSE_2_2", 0 ,"C-101-fuse-2_Takt",false};	
+	{"C-101-FUSE_2_2",ROUGHNESS_METALLIC,"C-101-fuse-2-2-CC_RoughMet",true};	
+-----------numeral "WING" CHILE/HONDURAS VERSION----------------
+	{"C-101-FUSE_2_3", 0 ,"C-101-fuse-2_Takt",false};--FIRST DIGIT
+	{"C-101-FUSE_2_3",ROUGHNESS_METALLIC,"C-101-fuse-2-2-CC_RoughMet",true};	
+	{"C-101-FUSE_2_3", DECAL ,"empty",true};
+	{"C-101-FUSE_2_4", 0 ,"C-101-fuse-2_Takt",false};--SECOND DIGIT
+	{"C-101-FUSE_2_4",ROUGHNESS_METALLIC,"C-101-fuse-2-2-CC_RoughMet",true};	
+	{"C-101-FUSE_2_4", DECAL ,"empty",true};
+	
+-----------Aluminium effect VERSION----------------------------
+	{"C-101-FUSE_2_5", 0 ,"C-101-fuse-2_Takt",true};--SECOND DIGIT
+	{"C-101-FUSE_2_5",ROUGHNESS_METALLIC,"C-101-fuse-2-2-CC_RoughMet",true};	
+----------------------------------------------------------------------PILOT----------------------------------------------------------------------
+-----front-----
+	{"pilot_C101_00_HELMET", 0 ,"pilot_c-101_helmet_GREY",true};
+	{"pilot_C101_00_HELMET", DECAL ,"empty",true};			
+	{"pilot_C101_00_body", 0 ,"pilot_c-101_00_GREEN",true};
+		{"pilot_C101_00_body", ROUGHNESS_METALLIC ,"pilot_c-101_00_RED_RoughMet",true};	
+	{"pilot_C101_patch", 0 ,"pilot_c-101_patch_USA",true};
+	
+	{"pilot_C101_00_HELMET",ROUGHNESS_METALLIC,"pilot_c-101_helmet_GREY_RoughMet",true};	
+-----rear-----	
+	{"pilot_C101_00_HELMET_1", 0 ,"pilot_c-101_helmet_GREY",true};
+	{"pilot_C101_00_body_1", 0 ,"pilot_c-101_00_GREEN",true};
+	{"pilot_C101_00_body_1", ROUGHNESS_METALLIC ,"pilot_c-101_00_RED_RoughMet",true};		
+	{"pilot_C101_patch_1", 0 ,"pilot_c-101_patch_USA",true};
+	
+	{"pilot_C101_00_HELMET_1",ROUGHNESS_METALLIC,"pilot_c-101_helmet_GREY_RoughMet",true};	
+	
+----------------------------------------------------------------------GUN----------------------------------------------------------------------
+-----DEFA553-----
+	{"C-101-DEFA553", 0 ,"C-101-GUN-2",true};
+	{"C-101-DEFA553",ROUGHNESS_METALLIC,"C-101-GUN-1_RoughMet",true};
+	
+-----AN-M3-----
+	{"C-101-AN-M3", 0 ,"AN_M3_1",true};		
+	{"C-101-AN-M3",ROUGHNESS_METALLIC,"AN_M3_RoughMet",true};		
+
+}
+name = "TaktLwG 66"
+
+
+custom_args = 
+{
+	[70] = 0.5, -- Tuskegee airmen head
+}
