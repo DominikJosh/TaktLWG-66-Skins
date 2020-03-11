92828de6431594e59e9ec5efbba07b3de7ab70f6
DominikJosh <42120789+DominikJosh@users.noreply.github.com>
2020-03-11 17:30:34 +0100

    JF-17-Skin Upload

---
+++ Eagle Dynamics/DCS World Openbeta/CoreMods/aircraft/ChinaAssetPack/Liveries/JF-17/TaktLwG 66/description.lua
@@ -0,0 +1,73 @@
+livery = {
+    {"JF-17_main01", 0, "JF-17_main01_takt", false},
+	{"JF-17_main01", ROUGHNESS_METALLIC ,"JF-17_main01_PLA01_roughmet.dds",false},
+    {"JF-17_main01_BN52", 0, "JF-17_main01_takt", false},
+	{"JF-17_main01_BN52", ROUGHNESS_METALLIC ,"JF-17_main01_PLA01_roughmet.dds",false},
+    {"JF-17_main01_BN31", 0, "JF-17_main01_takt", false},
+	{"JF-17_main01_BN31", ROUGHNESS_METALLIC ,"JF-17_main01_PLA01_roughmet.dds",false},
+    {"JF-17_main01_BN32", 0, "JF-17_main01_takt", false},
+	{"JF-17_main01_BN32", ROUGHNESS_METALLIC ,"JF-17_main01_PLA01_roughmet.dds",false},
+    {"JF-17_main01_BN52", 3, "JF-17_numbers02", false},
+    {"JF-17_main01_BN31", 3, "JF-17_numbers02", false},
+    {"JF-17_main01_BN32", 3, "JF-17_numbers02", false},
+    {"JF-17_main02", 0, "JF-17_main02_takt", false},
+	{"JF-17_main02", ROUGHNESS_METALLIC ,"JF-17_main02_PLA01_roughmet.dds",false},
+    {"JF-17_main03", 0, "JF-17_main03_takt", false},
+	{"JF-17_main03", ROUGHNESS_METALLIC ,"JF-17_main03_PLA01_roughmet.dds",false},
+    {"JF-17_main04", 0, "JF-17_main04_takt", false},
+	{"JF-17_main04", ROUGHNESS_METALLIC ,"JF-17_main04_PLA01_roughmet.dds",false},
+    {"JF-17_main05", 0, "JF-17_main05_takt", false},
+	{"JF-17_main05", ROUGHNESS_METALLIC ,"JF-17_main05_PLA01_roughmet.dds",false},
+    {"JF-17_main06", 0, "JF-17_main06_takt", false},
+	{"JF-17_main06", ROUGHNESS_METALLIC ,"JF-17_main06_PLA01_roughmet.dds",false},
+    {"JF-17_main07", 0, "JF-17_main07_takt", false},
+	{"JF-17_main07", ROUGHNESS_METALLIC ,"JF-17_main07_PLA01_roughmet.dds",false},
+    {"JF-17_main08", 0, "JF-17_main08_takt", false},
+	{"JF-17_main08", ROUGHNESS_METALLIC ,"JF-17_main08_PLA01_roughmet.dds",false},
+	{"JF-17_main08_decol", 0, "JF-17_main08_takt", false},
+	{"JF-17_main08_decol", ROUGHNESS_METALLIC ,"JF-17_main08_PLA01_roughmet.dds",false},
+	{"JF-17_main08_decol", 3, "empty", true},
+	{"JF-17_main08_BN52", 0, "JF-17_main08_takt", false},
+	{"JF-17_main08_BN52", ROUGHNESS_METALLIC ,"JF-17_main08_PLA01_roughmet.dds",false},
+	{"JF-17_main08_BN31", 0, "JF-17_main08_takt", false},
+	{"JF-17_main08_BN31", ROUGHNESS_METALLIC ,"JF-17_main08_PLA01_roughmet.dds",false},
+	{"JF-17_main08_BN32", 0, "JF-17_main08_takt", false},
+	{"JF-17_main08_BN32", ROUGHNESS_METALLIC ,"JF-17_main08_PLA01_roughmet.dds",false},
+	{"JF-17_main08_BN52", 3, "empty", true},
+	{"JF-17_main08_BN31", 3, "empty", true},
+	{"JF-17_main08_BN32", 3, "empty", true},
+    {"JF-17_main09", 0, "JF-17_main09_takt", false},
+	{"JF-17_main09", ROUGHNESS_METALLIC ,"JF-17_main09_PLA01_roughmet.dds",false},
+    {"JF-17_numbers02_BN52", 0, "empty", true},
+	{"JF-17_numbers02_BN31", 0, "empty", true},
+	{"JF-17_numbers02_BN32", 0, "empty", true},
+	{"BRU_33A", DIFFUSE, "bru_33a_dif", false};
+	{"BRU_33A", NORMAL_MAP, "bru_33a_nm", true};
+	{"BRU_33A", ROUGHNESS_METALLIC, "bru_33a_dif_roughmet", true};
+	{"BRU_33AA", DIFFUSE, "bru_33aa_diff", false};
+	{"BRU_33AA", NORMAL_MAP, "bru_33aa_nm", true};
+	{"BRU_33AA", ROUGHNESS_METALLIC, "bru_33aa_diff_roughmet", true};
+	{"Pylon_PF12Twin", 0, "jf-17_dual_pf", false};
+	{"Pylon_PF12Twin", ROUGHNESS_METALLIC, "JF-17_Dual_PF_RoughMet", false};
+	{"Pylon_PF-12", 0, "jf-17_pf12dual", false};
+	{"Pylon_PF-12", ROUGHNESS_METALLIC, "JF-17_PF12Dual_RoughMet", false};
+	{"Tank_800", DIFFUSE, "tank800", false};
+	{"Tank_800", NORMAL_MAP, "tank800_norm", false};
+	{"Tank_800", ROUGHNESS_METALLIC, "tank800_roughmet", false};
+	{"Tank_1100", DIFFUSE, "tank1100", false};
+	{"Tank_1100", NORMAL_MAP, "tank1100_norm", false};
+	{"Tank_1100", ROUGHNESS_METALLIC, "tank1100_roughmet", false};
+	{"WMD7_Body", DIFFUSE, "wmd7", false};
+	{"WMD7_Body", NORMAL_MAP, "wmd7_norm", true};
+	{"WMD7_Body", ROUGHNESS_METALLIC, "wmd7_roughmet", true};
+	{"WMD7_Glass", DIFFUSE, "glass_wmd7", true};
+	{"WMD7_Glass", ROUGHNESS_METALLIC, "glass_wmd7_roughmet", true};
+	{"DLPodAKG_main", DIFFUSE, "DLPodAKG", false};
+	{"DLPodAKG_main", NORMAL_MAP, "dlpodakg_norm", true};
+	{"DLPodAKG_main", ROUGHNESS_METALLIC, "dlpodakg_roughmet", true};
+	{"03 - Default", DIFFUSE, "DLPodAKG", false};
+	{"03 - Default", NORMAL_MAP, "dlpodakg_norm", true};
+	{"03 - Default", ROUGHNESS_METALLIC, "dlpodakg_roughmet", true};
+}
+name = "TaktLwG66"
+
