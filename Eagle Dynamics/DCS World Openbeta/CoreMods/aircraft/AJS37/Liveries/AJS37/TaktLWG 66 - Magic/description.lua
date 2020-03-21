8b461db737e3761ca75b04474900d2be46525dca
DominikJosh <42120789+DominikJosh@users.noreply.github.com>
2020-03-21 10:56:40 +0100

    Liveries für Magic nachgereicht

---
+++ Eagle Dynamics/DCS World Openbeta/CoreMods/aircraft/AJS37/Liveries/AJS37/TaktLWG 66 - Magic/description.lua
@@ -0,0 +1,21 @@
+livery = {
+
+
+--Diffuse  -----------------------------------
+{"LNS_VIG_EXT_FUS_T", 0 ,"LNS_VIG_FUSELAGE_T",false};
+{"LNS_VIG_EXT_FUS_L", 0 ,"LNS_VIG_FUSELAGE_L",false};
+{"LNS_VIG_EXT_FUS_R", 0 ,"LNS_VIG_FUSELAGE_R",false};
+{"LNS_VIG_EXT_WING_L", 0 ,"LNS_VIG_WING_L",false};
+{"LNS_VIG_EXT_WING_R", 0 ,"LNS_VIG_WING_R",false};
+
+--Pylons -------------------------------------
+{"LNS_VIG_PYLONS_02", 0 ,"Pilon_01",false};
+{"LNS_VIG_Pylons_01", 0 ,"Pilon_02",false};
+{"LNS_VIG_WING_PYLONS", 0 ,"Pilon_03",false};
+{"LNS_VIG_OUTER_PYLONS ", 0 ,"Pilon_04",false};
+
+{"AGM_65B", DIFFUSE, "RB75_Diff", true};	
+}
+
+name = "TaktLWG 66 - Magic"
+
