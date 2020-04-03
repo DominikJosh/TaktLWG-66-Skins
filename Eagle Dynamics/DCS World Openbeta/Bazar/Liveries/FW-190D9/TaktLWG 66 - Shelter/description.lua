5e1e0cd3432c81c5d190103ff9c79ac95a27d8a6
DominikJosh <42120789+DominikJosh@users.noreply.github.com>
2020-04-03 11:05:41 +0200

    Bf-109 und FW-190D9 Upload

---
+++ Eagle Dynamics/DCS World Openbeta/Bazar/Liveries/FW-190D9/TaktLWG 66 - Shelter/description.lua
@@ -0,0 +1,27 @@
+-- Created by Abburo
+livery = {
+	{"FW-190_01", 0 ,"FW_190_Rear-Fus.dds",false};
+	{"FW-190_02", 0 ,"FW_190_Front-Fus.dds",false};
+	{"FW-190_03", 0 ,"FW_190_Wing_L.dds",false};
+	{"FW-190_04", 0 ,"FW_190_Wing_R.dds",false};
+    
+	--{"FW_190_01", 1 ,"FW-190_NM_01",true};
+	--{"FW_190_02", 1 ,"FW-190_NM_02",true};
+	--{"FW_190_03", 1 ,"FW-190_NM_03",true};
+	--{"FW_190_04", 1 ,"FW-190_NM_04",true};
+	
+	{"FW-190_BORT_NUMBER", 0 ,"empty",true};
+        {"FW-190_BORT_NUMBER", DECAL ,"empty",true};
+	{"FW-190_NUMBER_GB", 0 ,"empty",true};
+        {"FW-190_NUMBER_GB", DECAL ,"empty",true};
+		
+		
+	{"FW-190_01", ROUGHNESS_METALLIC ,"FW-190_01_RoughMet",true};
+	{"FW-190_02", ROUGHNESS_METALLIC ,"FW-190_02_RoughMet",true};
+	{"FW-190_03", ROUGHNESS_METALLIC ,"FW-190_03_RoughMet",true};
+	{"FW-190_04", ROUGHNESS_METALLIC ,"FW-190_04_RoughMet",true};
+		
+		
+}
+name = " TaktLwG 66 - Shelter"
+
