1155d7126aac9d1f0b044bcbe06af10bee437fd9
DominikJosh <42120789+DominikJosh@users.noreply.github.com>
2020-05-11 17:58:51 +0200

    Stunt

---
+++ Eagle Dynamics/DCS World Openbeta/Bazar/Liveries/Bf-109K-4/TaktLwG66 - Stunt/description.lua
@@ -0,0 +1,35 @@
+-- By Maler
+livery = {
+	{"Bf-109K-4_main", 0 ,"Bf-109K-4_DIF_Takt",false};
+	{"Bf-109K-4_main", 1 ,"Bf-109K-4_Bump",true};
+	{"Bf-109K-4_main", ROUGHNESS_METALLIC ,"Bf-109K-4_DIF_RoughMet",true};
+	
+	
+	
+	
+	
+	{"Bf-109K-4_BORT_NUMBER_0", 0 ,"Bf-109K-4_DIF_Takt",false};
+	{"Bf-109K-4_BORT_NUMBER_0", 1 ,"Bf-109K-4_Bump",true};
+	{"Bf-109K-4_BORT_NUMBER_0", ROUGHNESS_METALLIC ,"Bf-109K-4_DIF_RoughMet",true};
+	
+	{"Bf-109K-4_BORT_NUMBER_0", DECAL ,"Bf-109K-4_bort_number_Green",false};
+	
+	
+	{"Bf-109K-4_BORT_NUMBER_1", 0 ,"Bf-109K-4_DIF_Takt",false};
+	{"Bf-109K-4_BORT_NUMBER_1", 1 ,"Bf-109K-4_Bump",true};
+	{"Bf-109K-4_BORT_NUMBER_1", ROUGHNESS_METALLIC ,"Bf-109K-4_DIF_RoughMet",true};
+	
+	{"Bf-109K-4_BORT_NUMBER_1", DECAL ,"Bf-109K-4_bort_number_Green",false};
+
+
+	{"Bf-109K-4_KIL_CROSS", 0 ,"Bf-109K-4_DIF_Takt",false};
+	{"Bf-109K-4_KIL_CROSS", 1 ,"Bf-109K-4_Bump",true};
+	{"Bf-109K-4_KIL_CROSS", ROUGHNESS_METALLIC ,"Bf-109K-4_DIF_RoughMet",true};
+	
+	{"Bf-109K-4_KIL_CROSS", DECAL ,"empty", true};
+	
+
+	
+}
+name = "TaktLwG 66 - Stunt"
+
