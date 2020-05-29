8abd18fdd6a845f1506ec65298a9d85692e12923
DominikJosh <42120789+DominikJosh@users.noreply.github.com>
2020-05-29 16:34:00 +0200

    Update

---
+++ Eagle Dynamics/DCS World Openbeta/CoreMods/aircraft/F-16C/Liveries/F-16C_50/TaktLWG 66 - Hinkel/description.lua
@@ -0,0 +1,39 @@
+livery = {
+	
+	{"F16_bl50_wing_L", 0, "f16_bl50_wing_l", false};
+	{"F16_bl50_wing_L", ROUGHNESS_METALLIC, "f16_bl50_wing_l_roughmet", false};
+	
+	{"F16_bl50_wing_R", 0, "f16_bl50_wing_r", false};
+	{"F16_bl50_wing_R", ROUGHNESS_METALLIC, "f16_bl50_wing_r_roughmet", false};
+	
+	{"F16_bl50_Main_1", 0, "f16_bl50_main_1", false};
+	{"F16_bl50_Main_1", ROUGHNESS_METALLIC, "f16_bl50_main_1_roughmet", false};
+	
+	{"F16_bl50_Main_2",	0, "f16_bl50_main_2", false};
+	{"F16_bl50_Main_2",	ROUGHNESS_METALLIC,	"f16_bl50_main_2_roughmet",	false};
+	
+	{"F16_bl50_Main_3",	0, "f16_bl50_main_3", false};
+	{"F16_bl50_Main_3",	ROUGHNESS_METALLIC,	"f16_bl50_main_3_roughmet",	false};
+	
+	{"F16_bl50_Kil", 0, "f16_bl50_kil", false};
+	{"F16_bl50_Kil", ROUGHNESS_METALLIC, "f16_bl50_kil_roughmet", false};
+	
+	{"F16_bl50_Wing_Pylon_1", 0, "f16_bl50_wing_pylon_1", false};
+	{"F16_bl50_Wing_Pylon_1", ROUGHNESS_METALLIC, "f16_bl50_wing_pylon_1_roughmet",	true};
+	
+	{"F16_bl50_Wing_Pylon_2", 0, "f16_bl50_wing_pylon_2", false};
+	{"F16_bl50_Wing_Pylon_1", ROUGHNESS_METALLIC, "f16_bl50_wing_pylon_1_roughmet",	true};
+	
+	{"LAU_129", 0, "LAU_129_diff", false};
+	{"LAU_129", ROUGHNESS_METALLIC, "LAU_129_diff_RoughMet", true}; 
+	
+	{"Fuel_Tank_300Gal", 0, "Fuel_Tank_300Gal", false};
+	{"Fuel_Tank_300Gal", ROUGHNESS_METALLIC, "Fuel_Tank_300Gal_RoughMet", false};
+	
+	{"F_16_Tank_370", 0, "F-16_Tank_370_diff", false};
+	{"F_16_Tank_370", ROUGHNESS_METALLIC, "F-16_Tank_370_diff_RoughMet", false};
+	
+	
+}
+name = "TaktLWG 66 - Hinkel"
+
